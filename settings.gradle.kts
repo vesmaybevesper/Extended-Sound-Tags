dcdb57efbf64e899df3ab2a004450ca696fd6e9b
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-08 11:41:02 -0400

    remove forge 1.21 and 26.x
    cant support these versions bc neoforge doesnt supply them as legacy versions and forge gradle proper is still on gradle 8 which massively outdate

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,8 +23,8 @@ stonecutter {
 		fun match(version: String, vararg loaders: String) =
 			loaders.forEach { version("$version-$it", version).buildscript = "build.$it.gradle.kts" }
 
-		match("26.1", "fabric", "neoforge", "forge")
-		match("1.21", "fabric", "neoforge", "forge")
+		match("26.1", "fabric", "neoforge")
+		match("1.21", "fabric", "neoforge")
 		match("1.20", "fabric", "forge")
 		match("1.19", "fabric", "forge")
 		match("1.18", "fabric", "forge")
